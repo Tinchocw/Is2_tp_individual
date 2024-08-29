3fe3b7d747905615d0cbb1b68284a5b0ca340bb2
Tinchocw <mcwikla@fi.uba.ar>
2024-08-29 00:23:11 -0300

    ADD Variables de entorno, dependencias y dokerfile

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.10.12
+
+WORKDIR /code
+
+COPY ./requirements.txt /code/requirements.txt
+
+
+RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+
+COPY ./app /code/app
+COPY ./.env /code/.env
+
+
+CMD ["python3", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
+import os
+
+import uvicorn
 from fastapi import FastAPI
-from app.src.router.router import Router
+from src.router.router import Router
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 router = Router().get_router()
 app.include_router(router)
 
+if __name__ == "__main__":
+    host = os.getenv("HOST")
+    port = int(os.getenv("PORT"))
+
+    uvicorn.run(app, host=host, port=port)
