47c2c1a9e493979fa0ecca2a94f2eb650d074746
Tinchocw <mcwikla@fi.uba.ar>
2024-08-31 19:43:15 -0300

    REF Quito los logs de la termina
    Los dejo unicamente en un archivo

--- app/main.py
+++ app/main.py
@@ -11,7 +11,6 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[
                         logging.FileHandler("app.log"),
-                        logging.StreamHandler()
                     ])
 
 logging.info("Application started")
